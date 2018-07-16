db1a06d0fcfe4f23fa44606f6252fb34faf15036
Mark Everett <themynnx@gmail.com>
2018-07-15 22:10:42 -0700

    Added ability to add a transaction

--- server/app.py
+++ server/app.py
@@ -2,7 +2,7 @@ import os
 from datetime import datetime
 from string import Template
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
@@ -21,8 +21,21 @@ class Transaction(db.Model):
     recipient = db.Column(db.Unicode(255), nullable=False)
     when=db.Column(db.DateTime, default=datetime.utcnow)
 
-@app.route('/')
+@app.route('/all')
 def show_all():
     return render_template('show_all.html',
         transactions=Transaction.query.order_by(Transaction.when.desc()).all()
     )
+
+@app.route('/')
+def landing():
+    return render_template('landing.html')
+
+@app.route('/transact', methods=["POST"])
+def transact():
+    tran = Transaction( fuckcoin_id=request.form["fuckcoin_id"],
+            giver=request.form["giver"],
+            recipient=request.form["recipient"])
+    db.session.add(tran)
+    db.session.commit()
+    return redirect(url_for("show_all"))

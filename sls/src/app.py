c3ea43272eb20794b7f90dceaa5072cbd7eb0735
Mark Everett <themynnx@gmail.com>
2018-07-29 23:07:05 -0700

    it's aliiiive

--- sls/src/app.py
+++ sls/src/app.py
@@ -12,10 +12,43 @@ except Exception as e:
     print("need to pip install pycryptodome")
     raise
 
-from flask import Flask, render_template, request, redirect, url_for
+
+from flask import Flask, render_template, request, redirect, url_for, jsonify
 
 ecc_public_key = base64.b64decode("MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEqMI3pmvasBrNU9k1PyG+g56fnWSVrz8Y0zj9rY5XOlbN8hiQebEJ6ZD17nqjMoKcuzB80NCu7PoSzSBgkzq4Ig==")
 app = Flask(__name__)
+
+TRANSACTIONS_TABLE = os.environ['TRANSACTIONS_TABLE']
+IS_OFFLINE = os.environ.get('IS_OFFLINE')
+
+print(IS_OFFLINE)
+if IS_OFFLINE:
+    client = boto3.client(
+        'dynamodb',
+        region_name='localhost',
+        endpoint_url='http://localhost:8000'
+    )
+else:
+    client = boto3.client('dynamodb')
+
+class Transaction:
+    def __init__(self, fuckcoinId, giver, recipient, purpose, when):
+        self.fuckcoinId = fuckcoinId
+        self.giver = giver
+        self.recipient = recipient
+        self.purpose = purpose
+        self.when = when
+
+    @classmethod
+    def from_ddb(cls, ddb_resp):
+        fuckcoinId = ddb_resp['fuckcoinId']['S']
+        giver = ddb_resp['giver']['S']
+        recipient = ddb_resp['recipient']['S']
+        purpose = ddb_resp['purpose']['S']
+        when = ddb_resp['when']['S']
+
+        transaction = cls(fuckcoinId, giver, recipient, purpose, when)
+        return transaction
 
 import sys
 def frint(string):
@@ -23,24 +56,46 @@ def frint(string):
 
 @app.route('/all')
 def show_all():
-    transactions = [
-        {'fuckcoin_id': 20, 'giver': 'Mark', 'recipient': 'David', 'purpose': 'stuffs'}
-    ]
-
-    return render_template('show_all.html', transactions=transactions)
+    resp = client.scan(TableName=TRANSACTIONS_TABLE)
+    return jsonify(resp)
 
 @app.route('/coin')
 def landing():
     coinNumber = request.args.get('sn')
     signature = request.args.get('sig', None)
-    if coinNumber and signature:
-        frint(coinNumber, signature, isLegit)
+
+    resp = client.query(
+        TableName=TRANSACTIONS_TABLE,
+        KeyConditionExpression="fuckcoinId = :fuckcoinId",
+        ExpressionAttributeValues={
+            ":fuckcoinId": {'S': coinNumber}
+        }
+    )
+
+    frint(resp)
+    items = resp.get("Items");
+    items = [Transaction.from_ddb(item) for item in items]
+
+    if signature:
+        frint(coinNumber)
+        frint(signature)
         isLegit = verify_sig(coinNumber, signature)
 
-    return render_template('landing.html', isLegit=isLegit, coin=coinNumber)
+    return render_template('show_all.html', transactions=items)
 
 @app.route('/transact', methods=["POST"])
 def transact():
+    resp = client.put_item(
+            TableName=TRANSACTIONS_TABLE,
+            Item={
+                'fuckcoinId': {'S': request.form["fuckcoin_id"] },
+                'giver': {'S': request.form["giver"] },
+                'recipient': {'S': request.form["recipient"] },
+                'purpose': {'S': request.form["purpose"] },
+                'when': {'S': datetime.utcnow().isoformat() }
+            }
+        )
+
     return redirect(url_for("show_all"))
 
 def verify_sig(sn, b64sig):
